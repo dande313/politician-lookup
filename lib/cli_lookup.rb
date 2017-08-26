23e220d6a5020e215a09ce643db2f00a1f9fca41
Allen Anderson <dande313@byu.net>
2017-08-26 00:29:32 -0400

    Added intro, accepts zip code input, only if a string of 5 digits. May lead with a 0.

--- lib/cli_lookup.rb
+++ lib/cli_lookup.rb
@@ -4,7 +4,41 @@ class Lookup
 	def initialize
 	end
 
-	def hello_world
-		puts "Hello World!"
+    def clear_screen
+    	puts "\e[H\e[2J"
+    end
+
+	def introduction
+		puts ""
+	    puts "************* US Representative Lookup *************"
+	    puts "                      v.01                          "
+	    puts "                by Allen Anderson                   "
+	    puts ""
+	    puts ""
 	end
+
+	def valid_zip_code?(zip_code)
+  		zip_code.size == 5 && zip_code.scan(/\D/).empty?
+	end
+
+	def get_input
+		puts " Please enter your five-digit ZIP Code:"
+		zip_code = gets.chomp
+		if valid_zip_code?(zip_code)
+			return zip_code
+		else
+			clear_screen
+			run
+		end
+	end
+
+
+	def run
+		clear_screen
+		introduction
+		zip_code = get_input
+		puts "Yay! You entered a REAL zip code! It is #{zip_code}."
+		exit
+	end
+
 end
